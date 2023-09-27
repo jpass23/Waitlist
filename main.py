8068b0b707a007919a5bffe89aa8949e26ab1e83
jpass23 <jpass23@live.unc.edu>
2023-09-26 21:30:48 -0400

    Add join and waitlist frontend
    `join.html` contains the code for displaying the frontend for the join page that gets served at `/join-waitlist`. The `/add` endpoint has been changed to serve the `waitlist.html` file after adding the person to the database. A repeated attempt to add an existing member to the waitlist (aka reloading the page) will not cause issues.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from typing import Union
 from deta import Deta
 from models import FetchResponse, Customer
@@ -14,8 +16,14 @@ queue = deta.Base("queue")
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 def myHash(cust: Customer) -> str:
     return str(floor(abs(hash(cust.name)/(10**7))) + floor(abs(hash(cust.number)/(10**4))))
+
+@app.get("/join-waitlist", response_class=HTMLResponse)
+def render_join_page(request: Request):
+    return templates.TemplateResponse("join.html", {"request": request})
 
 @app.get("/")
 def read_root():
@@ -23,8 +31,11 @@ def read_root():
 
 @app.get("/customers")
 def get_customers() -> list[Customer]:
-    table: FetchResponse = db.fetch()
-    return table.items
+    keys = get_queue()
+    customers = []
+    for key in keys:
+        customers.append(get_customer_for_key(key))
+    return customers
 
 @app.get("/queue")
 def get_queue() -> list[int]:
@@ -51,15 +62,18 @@ def advance_queue() -> Union[Customer, None]:
     db.delete(nextCustKey)
     return customer
 
-@app.get("/add")
-def add_customer(name: str, number: int) -> Customer:
+@app.get("/add", response_class=HTMLResponse)
+def add_customer(request: Request, name: str, number: int) -> Customer:
     customer: Customer = Customer(name=name, number=number)
     key = myHash(customer)
-    currentQueue = queue.get("0")["list"]
-    currentQueue.append(key)
+    currentQueue: list = queue.get("0")["list"]
+    if key in currentQueue:
+        pass
+    else:
+        currentQueue.append(key)
     queue.put({"list" :currentQueue}, "0")
-    return db.put(customer.toDict(), key) #, expire_in=43200) #Expires in 12 hours
-
+    db.put(customer.toDict(), key) #, expire_in=43200) #Expires in 12 hours
+    return templates.TemplateResponse("waitlist.html", {"request": request, "name": name, "number": number})
 
 @app.get("/tablesize")
 def get_num_customers() -> int:
